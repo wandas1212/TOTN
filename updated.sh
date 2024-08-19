64a991b7a15ad3c8adc76f2502b62b81f03987f8
wandas1212 <76051210+wandas1212@users.noreply.github.com>
2024-08-19 03:10:36 -0400

    Update updated.sh

--- updated.sh
+++ updated.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 username=$(whoami)
-export XAUTHORITY=/home/$username/.Xauthority
+current_dir=$(pwd)
+export XAUTHORITY=/home/wandas/.Xauthority
 export DISPLAY=:0
+
+#echo $XAUTHORITY
+#echo $DISPLAY
+
 
 
 is_prompt_shown=false
@@ -11,10 +16,25 @@ while true; do
     #up_iface=$(ip -o link show | awk -F': ' '{print $2}' | xargs -I {} sh -c 'ip address show dev {} | grep -q "state UP" && echo {}' | head -n 1)
     up_iface=$(ip -o link show | awk -F': ' '{print $2}' | grep -E '^(eth|wl|en|wlp)' | xargs -I {} sh -c 'ip address show dev {} | grep -q "state UP" && echo {}' | head -n 1)
 
+
     # Check if any network interface is up
     if [ -n "$up_iface" ]; then
     #bssid=$(iwconfig wlan0 | awk '/Access Point/ {print $NF}')
-    net_name=$(nmcli -t -f active,ssid dev wifi | grep '^yes' | cut -d':' -f2 | tr ' ' '_')
+    
+    	#network name
+    network_name(){
+	if [[ "$up_iface" == *"wl"* ]]; then
+    # Wireless interface (Wi-Fi)
+    net_name=$(nmcli -t -f active,ssid dev wifi | grep '^yes' | cut -d':' -f2 | tr ' ' '_' || echo "unknown_wifi")
+    else
+    # Wired interface (Ethernet)
+    net_name=$(ip -o -4 addr show $up_iface | awk '{print $4}' | cut -d'/' -f1 | tr ' ' '_' || echo "unknown_ethernet")
+    fi
+    }
+    
+    network_name
+    
+    #net_name=$(nmcli -t -f active,ssid dev wifi | grep '^yes' | cut -d':' -f2 | tr ' ' '_')
 
         if ! $is_prompt_shown; then
             is_prompt_shown=true
@@ -31,7 +51,7 @@ while true; do
                     choice=$(echo "$choice" | tr "[:upper:]" "[:lower:]")  # Convert to lowercase
 
                     if [ "$choice" = "y" ]; then
-                        cd /home/wandas/Desktop/TOTN/ && ./test.sh
+			cd /home/wandas/Desktop/TOTN && ./test.sh
                         #sleep 3
                     elif [ "$choice" != "n" ]; then
                         # Exit the program
